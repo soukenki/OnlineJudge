7973107244b6b8f4b3457d44d34d98918ad7437e
soukenki <1106671528@qq.com>
2023-10-01 15:24:19 +0000

    10/1 更新编译部分

--- comm/util.hpp
+++ comm/util.hpp
@@ -4,6 +4,12 @@
 #include <iostream>
 #include <string>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+
 namespace ns_util
 {
     const std::string temp_path = "./temp/";  // 路径
@@ -11,22 +17,48 @@ namespace ns_util
     class PathUtil
     {
     public:
+        // 加后缀
+        // 123 -> ./temp/123.cpp
+        static std::string AddSuffix(const std::string &file_name, const std::string &suffix)
+        {
+            std::string path_name = temp_path;
+            path_name += file_name;
+            path_name += suffix;
+            return path_name;
+        }
+
         // 构建源文件路径，并加后缀的完整文件名
         static std::string Src(const std::string &file_name)
         {
-            
+            return AddSuffix(file_name, ".cpp");
         }
 
         // 构建可执行程序的完整路径，并加后缀名
         static std::string Exe(const std::string &file_name)
         {
-
+            return AddSuffix(file_name, ".exe");
         }
 
         // 构建该程序标准错误的完整路径，并加后缀名
         static std::string Stderr(const std::string &file_name)
         {
+            return AddSuffix(file_name, ".stderr");
+        }
+    };
 
+
+    class FileUtil
+    {
+    public:
+        static bool IsFileExists(const std::string &path_name)
+        {
+            struct stat st;
+            if (stat(path_name.c_str(), &st) == 0)
+            {
+                // 获取属性成功，文件存在
+                return true;
+            }
+            return false;
         }
     };
 }
--- compile_server/compiler.hpp
+++ compile_server/compiler.hpp
@@ -3,7 +3,12 @@
 #pragma once
 
 #include <iostream>
+
+#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 #include "../comm/util.hpp"
 
@@ -30,23 +35,42 @@ namespace ns_compiler
         // 111 -> ./temp/111.stderr
         static bool Compile(const std::string &file_name)
         {
-            pid_t res = fork();
-            if (res < 0)
+            pid_t pid = fork();
+            if (pid < 0)
             {
                 return false;
             }
-            else if (res == 0)
+            else if (pid == 0)
             {
+                // 打开文件，把错误信息重定向到文件
+                int _stderr = open(PathUtil::Stderr(file_name).c_str(), O_CREAT | O_WRONLY, 0644);  
+                if (_stderr < 0)
+                {
+                    exit(1);
+                }
+                // 重定向标准错误到_stderr
+                dup2(_stderr, 2);  // 2重定向到文件
+                
+
                 // 子进程：调用编译器，完成对代码的编译工作
                 // g++ -o target src -std=c++11
                 execlp("g++", "-o", PathUtil::Exe(file_name).c_str(), \
-                        PathUtil::Src(file_name).c_str(), "-std=c++11", nullptr); // 进程替换, 结尾nullptr不要忘
+                        PathUtil::Src(file_name).c_str(), "-std=c++11", nullptr); // 进程替换, 结尾nullptr不要忘，程序替换不影响文件描述符表
 
+                exit(2);
             }
             else
             {
-                //父进程
+                // 父进程
+                waitpid(pid, nullptr, 0);
+                // 编译是否成功，看是否形成可执行文件
+                if (FileUtil::IsFileExists(PathUtil::Exe(file_name)))
+                {
+                    return true; // 编译成功，运行文件存在
+                }
             }
+
+            return false;  // 编译失败，运行文件不在
         }
     
     private:
